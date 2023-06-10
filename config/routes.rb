7e4fa35a446157d567db91bfe5397c8c8a21c4a3
Rachel Isaac <rachelisaac995@gmail.com>
2023-06-10 14:31:52 +0400

    Set up the routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do  
+  resources :greetings
+  end  
+  
+  root 'root#index'
 end
