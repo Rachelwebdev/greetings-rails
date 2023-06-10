14dfd3ddcbc838786b1bb024b0a3271c0c255124
Rachel Isaac <rachelisaac995@gmail.com>
2023-06-10 15:13:46 +0400

    Add cors gem

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'  # Update with your frontend origin
-    resource '*', headers: :any, methods: [:get] # Update with the desired HTTP methods
+    origins 'http://127.0.0.1:5000' , 'http://localhost:3000','http://localhost:3001' # Update with your frontend origin
+    resource'*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],credentials: true # Update with the desired HTTP methods
   end
 end
+
+
